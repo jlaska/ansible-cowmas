879f8cd7c39bebecc66d424379a9638af1e1fc4c
James Laska <jlaska@redhat.com>
2017-12-12 16:18:07 -0500

    Implement Display class with Colored light banner

--- callback_plugins/cowmas.py
+++ callback_plugins/cowmas.py
@@ -1,6 +1,9 @@
+import random
 from ansible.plugins.callback.default import CallbackModule
 from ansible.utils.color import stringc
 from ansible import constants as C
+from ansible.utils.display import Display
+
 
 all_colors = [C.COLOR_CHANGED, C.COLOR_DEBUG, C.COLOR_DEPRECATE, C.COLOR_DIFF_ADD, C.COLOR_DIFF_LINES,
               C.COLOR_DIFF_REMOVE, C.COLOR_ERROR, C.COLOR_HIGHLIGHT, C.COLOR_OK, C.COLOR_SKIP, C.COLOR_UNREACHABLE,
@@ -10,11 +13,71 @@ all_colors = [C.COLOR_CHANGED, C.COLOR_DEBUG, C.COLOR_DEPRECATE, C.COLOR_DIFF_AD
 def colorize(txt, color=None):
     output = ''
     if color is None:
+        rand_start = random.randint(0, len(all_colors))
         for (idx, char) in enumerate(txt):
-            output += stringc(char, all_colors[idx % len(all_colors)])
+            # output += stringc(char, all_colors[idx % len(all_colors)])
+            output += stringc(char, random.sample(all_colors, 1)[0])
+            # output += stringc(char, all_colors[(rand_start + idx) % len(all_colors)])
     else:
         output += stringc(txt, color)
     return output
+
+
+def colorize_lights(light_len=3):
+    '''Convert white lights to colored lights of the desired length.'''
+    # Credit http://www.chris.com/ascii/index.php?art=holiday/christmas/other
+    # .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.
+
+    # size the lights appropriately
+    white_lights = u".:*~*:._" * light_len
+    white_lights = white_lights[:light_len - 8]
+
+    # swap out bulbs with some colored bulbs
+    colored_lights = ''
+    for light in white_lights:
+        if light in ['*', '_']:
+            colored_lights += colorize(light)
+        else:
+            colored_lights += light
+    return colored_lights
+
+# http://www.chris.com/ascii/index.php?art=holiday/christmas/other
+# .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.
+# -=[ The Twelve Days of Christmas ]=- 12/96
+# .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:.
+
+
+# http://www.chris.com/ascii/index.php?art=holiday/christmas/other
+#          .--._.--.--.__.--.--.__.--.--.__.--.--._.--.
+#        _(_      _Y_      _Y_      _Y_      _Y_      _)_
+#       [___]    [___]    [___]    [___]    [___]    [___]
+#       /:' \    /:' \    /:' \    /:' \    /:' \    /:' \
+#      |::   |  |::   |  |::   |  |::   |  |::   |  |::   |
+#      \::.  /  \::.  /  \::.  /  \::.  /  \::.  /  \::.  /
+#  jgs  \::./    \::./    \::./    \::./    \::./    \::./
+#        '='      '='      '='      '='      '='      '='
+
+class LightDisplay(Display):
+
+    def banner(self, msg, color=None, cows=True):
+        '''
+        Prints a holiday themed header depending on terminal width (3 minimum)
+        '''
+        if self.b_cowsay and cows:
+            try:
+                self.banner_cowsay(msg)
+                return
+            except OSError:
+                self.warning("somebody cleverly deleted cowsay or something during the PB run.  heh.")
+
+        msg = msg.strip()
+        light_len = max(3, self.columns - len(msg))
+        self.display(u"\n%s %s" % (msg, colorize_lights(light_len)))
+
+        # self.display(u"\n")
+        # self.display(u"%s" % colorize_lights())
+        # self.display(u"-= %s =-" % msg, color=color)
+        # self.display(u"%s" % colorize_lights())
 
 
 class CallbackModule(CallbackModule):
@@ -23,6 +86,10 @@ class CallbackModule(CallbackModule):
     CALLBACK_TYPE = 'stdout'
     CALLBACK_NAME = 'cowmas'
     CALLBACK_NEEDS_WHITELIST = True
+
+    def __init__(self, display=None, options=None):
+        super(CallbackModule, self).__init__()
+        self._display = LightDisplay()
 
     def v2_playbook_on_stats(self, stats):
         super(CallbackModule, self).v2_playbook_on_stats(stats)
@@ -33,6 +100,7 @@ class CallbackModule(CallbackModule):
         elif len(stats.dark):
             force_color = C.COLOR_UNREACHABLE
         else:
+            force_color = C.COLOR_HIGHLIGHT
             force_color = None
 
         print colorize('''
@@ -41,7 +109,7 @@ class CallbackModule(CallbackModule):
     \/ \/  \/ \/
  *    \/    \/      *
       (A)  (A)
-       \ ^^ / 			 *
+       \ ^^ /            *
        (o)(o)--)---------\.
        |    |          A  \\
         \__/             ,|  *
